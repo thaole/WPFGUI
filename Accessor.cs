458e323c1201d1fe15dd5e437b7a718ad101bf7c
thaole <hoangthaodn@gmail.com>
2014-08-04 16:11:24 +0700

    The third commit from thomas: using command line

--- Accessor.cs
+++ Accessor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Data.Linq;
 using System.Text;
 
-
+//thomas add some new command here
 
 namespace LINQToSQL
 {
---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// thomas also add a class to check status of git
+
+
+namespace L2S_Northwind
+{
+    class Class1
+    {
+    }
+}
