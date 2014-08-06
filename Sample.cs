9a6ead43cc9e7baf28a9ab80e77db8b90cda3b0e
thaole <hoangthaodn@gmail.com>
2014-08-06 09:54:42 +0700

    thomas add 9:54 - 6-Aug

--- Sample.cs
+++ Sample.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-// thomas also add a class to check status of git
-
-
 namespace L2S_Northwind
 {
-    class Class1
+    class Sample
     {
     }
 }
